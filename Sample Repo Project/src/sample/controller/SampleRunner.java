d8f04b12f05b1764ed064c38d1d5470bfe4f399f
Cody Henrichsen <cody.henrichsen@canyonsdistrict.org>
2013-09-23 14:14:59 -0600

    Created runner

---
+++ Sample Repo Project/src/sample/controller/SampleRunner.java
@@ -0,0 +1,18 @@
+package sample.controller;
+
+import sample.view.SampleFrame;
+
+public class SampleRunner
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		SampleFrame myApp = new SampleFrame();
+		myApp.start();
+
+	}
+
+}
